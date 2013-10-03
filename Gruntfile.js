ab3cb76d48447da8ef77c38e24ec43c2c0c08a8a
leftieFriele <leftie.friele@gmail.com>
2013-10-03 19:09:01 +0200

    adding the watch option for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,23 @@
 module.exports = function(grunt){
 
-	grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.initConfig({
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec'
-				},
-				src: ['src/**/*.js', 'test/**/*.js']
-			}
-		}
-	});
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        watch: {
+          files: ['src/**/*.js', 'test/**/*.js'],
+          tasks: ['mochaTest']
+        },
+    });
 
-	grunt.registerTask('default', 'mochaTest');
+    grunt.registerTask('default', 'mochaTest');
 
 };
