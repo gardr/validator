bcd9db5101dcf4330f4568c4f592798264737af3
sverosak <sveinung.rosaker@finn.no>
2013-11-12 19:32:27 +0100

    move wrap.js, add simple error hook test

--- test/rule/errors.test.js
+++ test/rule/errors.test.js
@@ -2,17 +2,79 @@ var buster = require('buster-assertions');
 var assert = buster.assert;
 var refute = buster.refute;
 
-var errorsHook = require('../../lib/rule/hook/errors.js');
-var errors     = require('../../lib/rule/validator/errors.js');
+var hook = require('../../lib/rule/hook/errors.js');
 
-describe('errors hook and validator', function(){
+describe('Errors hook', function () {
 
-    it.skip('should listen for window errors in window and in iframes', function(){
+    it('should inject error probe in pasties frame', function () {
+        var calls = 0;
+        var _window = {
+            'top': {
+                __errors: []
+            },
+            'onerror': function () {
+                calls++;
+            }
+        };
+        var api = {
+            'switchToIframe': function () {
+                calls++;
+            },
+            'evaluate': function (fn) {
+                global.window = _window;
+                calls++;
+                fn();
+                global.window = null;
+            }
+        };
+
+        hook.onNavigationRequested('invalid', null, null, null, api);
+
+        assert.equals(calls, 0);
+
+        var url = 'http://valid.no/page.html?param=param#PASTIES_' + Math.random();
+        hook.onNavigationRequested(url, null, null, null, api);
+
+        assert.equals(calls, 2);
+
+        _window.onerror('some error', 'url', 'lineNumber');
+
+        assert.equals(calls, 3);
+        assert.equals(_window.top.__errors.length, 1);
 
     });
 
-    it.skip('should report usererrors as validations erros', function(){
+});
 
+var validator = require('../../lib/rule/validator/errors.js');
+describe('Errors validator', function () {
+
+    it('should report usererrors as validations erros', function () {
+
+        var called = 0;
+        var report = {
+            'error': function(){
+                called++;
+            }
+        };
+
+        var harvested = {
+            systemErrors: [],
+            errors: []
+        };
+
+        validator.validate(harvested, report, function(){});
+        assert.equals(called, 0);
+
+
+        harvested.errors.push({message: 'some error'});
+        validator.validate(harvested, report, function(){});
+
+        assert.equals(called, 1);
+
+        harvested.systemErrors.push({message: 'another error'});
+        validator.validate(harvested, report, function(){});
+
+        assert.equals(called, 3);
     });
-
 });
