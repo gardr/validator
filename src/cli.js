7238c640e84fcaf1b9ec1ef92aff4751002f6ef5
sverosak <sveinung.rosaker@finn.no>
2013-11-12 10:13:41 +0100

    cleanup

--- src/cli.js
+++
@@ -1,9 +0,0 @@
-var argv = require('optimist')
-            .demand('url')
-            .alias('url', 'u')
-            .argv;
-var runner = require('./runner');
-
-console.log('Validating with input:', argv.url);
-
-console.log('Thank you, come again!');
