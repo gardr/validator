80080cdbbe8e3c641131e5b5e700063fffe46983
Sveinung Røsaker <sveinung.rosaker@gmail.com>
2013-12-02 14:28:19 +0100

    add har post-processing

--- test/rule/har.test.js
+++ test/rule/har.test.js
@@ -1,3 +1,7 @@
+var http = require('http');
+var xstatic = require('node-static');
+var hoek = require('hoek');
+
 var buster = require('buster-assertions');
 var assert = buster.assert;
 var refute = buster.refute;
@@ -33,6 +37,10 @@ describe('HAR hook', function () {
 });
 
 var proxyquire = require('proxyquire');
+var HARFile = require('./fixtures/HARFile.json');
+
+
+
 
 describe('HAR validator', function () {
 
@@ -54,8 +62,94 @@ describe('HAR validator', function () {
 
         validator.validate(harvested, null, function () {
             assert(harvested.HARFile);
-            assert.equals(harvested.HARFile.resources.length, harvested.harInput.resources.length);
+            assert.equals(0, harvested.harInput.resources.length);
             done();
         });
+    });
+
+
+
+    var server;
+    before(function(){
+
+        var path = __dirname + '/fixtures/files';
+        var file = new xstatic.Server(path);
+        var fileGzip = new xstatic.Server(path,{ gzip: true });
+
+        var gzipRegExp = /gzip=true/i;
+        var redirectsRegExp = /redirect=(\d+)/i;
+
+        server = http.createServer(function (request, response) {
+            request.addListener('end', function () {
+                if (redirectsRegExp.test(request.url)){
+                    var match = request.url.match(redirectsRegExp);
+                    var counter = parseInt(match[1], 10);
+                    var query = counter > 0 ? 'redirect='+(counter-1) : '';
+                    var newUrl = request.url.replace(redirectsRegExp, query);
+
+                    response.writeHead(302, { 'Location': newUrl });
+                    response.end();
+                }
+                else if (gzipRegExp.test(request.url)){
+                    fileGzip.serve(request, response);
+                }
+                else {
+                    file.serve(request, response);
+                }
+
+
+            }).resume();
+        }).listen();
+    });
+
+    function getHARfile(){
+        var har = hoek.clone(HARFile);
+        var host = 'http://localhost:'+server.address().port;
+
+        har.log.entries.forEach(function(entry){
+            entry.request.url = host + entry.request.url ;
+        });
+
+        return har;
+    }
+
+    it('should populate real sizes and collect contents', function(done){
+        var harvested = {
+            HARFile: getHARfile()
+        };
+
+        var host = 'http://localhost:'+server.address().port;
+        function get(key){
+            return harvested.rawFileData[host+key];
+        }
+
+        validator.getRealResources(harvested, null, function(){
+
+            assert.isObject(harvested.HARFile);
+
+            assert.isObject(harvested.rawFileData);
+            assert.equals(Object.keys(harvested.rawFileData).length, 5);
+
+            refute(get('/addyn.js').compressed);
+            assert(get('/addyn.js?gzip=true').compressed, 'gzip is on, so compressed flag should be true');
+            assert.equals(get('/addyn.js?redirect=5&gzip=true').redirects.length, 6);
+
+
+
+            assert.isObject(harvested.rawFileDataSummary);
+            assert.equals(harvested.rawFileDataSummary.total.redirects, 6);
+            assert.equals(harvested.rawFileDataSummary.total.rawRequests, 5);
+            assert.equals(harvested.rawFileDataSummary.total.requests, 11);
+
+            //console.log(harvested.rawFileDataSummary)
+
+            done();
+        });
+    });
+
+
+    after(function(){
+        server.close();
+        server = null;
     });
 });
